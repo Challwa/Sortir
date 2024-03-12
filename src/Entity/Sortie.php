2eb6ab191b62aa38d10b20f9143d1b571b1c672c
foreal2023 <fanny.oreal@gmail.com>
2024-03-12 09:55:58 +0100

    add entity sortie + crud twig

---
+++ src/Entity/Sortie.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity()]
+class Sortie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $idSortie;
+
+    #[ORM\Column(name:'id',type: Types::INTEGER, nullable: false, options: ['unsigned' => true], length: 30)]
+    private $nom;
+
+    #[ORM\Column(name: 'dateHeureDebut',type: Types::DATETIME_MUTABLE, nullable: false)]
+    private $dateHeureDebut;
+
+    #[ORM\Column(name: 'duree',type: Types::DATETIME_MUTABLE, nullable: false)]
+    private $duree;
+
+    #[ORM\Column(name: 'dateLimiteInscription',type: Types::DATETIME_MUTABLE, nullable: false)]
+    private $dateLimiteInscription;
+
+    #[ORM\Column(name: 'nbInscriptionsMax',type: Types::INTEGER, nullable: false)]
+    private $nbInscriptionsMax;
+
+    #[ORM\Column(name: 'infosSortie',type: Types::TEXT, nullable: false)]
+    private $infosSortie;
+
+    #[ORM\Column(name: 'etat',type: Types::STRING, nullable: false)]
+    private $etat;
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom): void
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateHeureDebut()
+    {
+        return $this->dateHeureDebut;
+    }
+
+    /**
+     * @param mixed $dateHeureDebut
+     */
+    public function setDateHeureDebut($dateHeureDebut): void
+    {
+        $this->dateHeureDebut = $dateHeureDebut;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDuree()
+    {
+        return $this->duree;
+    }
+
+    /**
+     * @param mixed $duree
+     */
+    public function setDuree($duree): void
+    {
+        $this->duree = $duree;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateLimiteInscription()
+    {
+        return $this->dateLimiteInscription;
+    }
+
+    /**
+     * @param mixed $dateLimiteInscription
+     */
+    public function setDateLimiteInscription($dateLimiteInscription): void
+    {
+        $this->dateLimiteInscription = $dateLimiteInscription;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNbInscriptionsMax()
+    {
+        return $this->nbInscriptionsMax;
+    }
+
+    /**
+     * @param mixed $nbInscriptionsMax
+     */
+    public function setNbInscriptionsMax($nbInscriptionsMax): void
+    {
+        $this->nbInscriptionsMax = $nbInscriptionsMax;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getInfosSortie()
+    {
+        return $this->infosSortie;
+    }
+
+    /**
+     * @param mixed $infosSortie
+     */
+    public function setInfosSortie($infosSortie): void
+    {
+        $this->infosSortie = $infosSortie;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEtat()
+    {
+        return $this->etat;
+    }
+
+    /**
+     * @param mixed $etat
+     */
+    public function setEtat($etat): void
+    {
+        $this->etat = $etat;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdSortie()
+    {
+        return $this->idSortie;
+    }
+
+
+
+}
---
+++ tests/Controller/SortieControllerTest.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace App\Test\Controller;
+
+use App\Entity\Sortie;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SortieControllerTest extends WebTestCase
+{
+    private KernelBrowser $client;
+    private EntityManagerInterface $manager;
+    private EntityRepository $repository;
+    private string $path = '/sortie/';
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+        $this->manager = static::getContainer()->get('doctrine')->getManager();
+        $this->repository = $this->manager->getRepository(Sortie::class);
+
+        foreach ($this->repository->findAll() as $object) {
+            $this->manager->remove($object);
+        }
+
+        $this->manager->flush();
+    }
+
+    public function testIndex(): void
+    {
+        $crawler = $this->client->request('GET', $this->path);
+
+        self::assertResponseStatusCodeSame(200);
+        self::assertPageTitleContains('Sortie index');
+
+        // Use the $crawler to perform additional assertions e.g.
+        // self::assertSame('Some text on the page', $crawler->filter('.p')->first());
+    }
+
+    public function testNew(): void
+    {
+        $this->markTestIncomplete();
+        $this->client->request('GET', sprintf('%snew', $this->path));
+
+        self::assertResponseStatusCodeSame(200);
+
+        $this->client->submitForm('Save', [
+            'sortie[nom]' => 'Testing',
+            'sortie[dateHeureDebut]' => 'Testing',
+            'sortie[duree]' => 'Testing',
+            'sortie[dateLimiteInscription]' => 'Testing',
+            'sortie[nbInscriptionsMax]' => 'Testing',
+            'sortie[infosSortie]' => 'Testing',
+            'sortie[etat]' => 'Testing',
+        ]);
+
+        self::assertResponseRedirects($this->path);
+
+        self::assertSame(1, $this->repository->count([]));
+    }
+
+    public function testShow(): void
+    {
+        $this->markTestIncomplete();
+        $fixture = new Sortie();
+        $fixture->setNom('My Title');
+        $fixture->setDateHeureDebut('My Title');
+        $fixture->setDuree('My Title');
+        $fixture->setDateLimiteInscription('My Title');
+        $fixture->setNbInscriptionsMax('My Title');
+        $fixture->setInfosSortie('My Title');
+        $fixture->setEtat('My Title');
+
+        $this->manager->persist($fixture);
+        $this->manager->flush();
+
+        $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+
+        self::assertResponseStatusCodeSame(200);
+        self::assertPageTitleContains('Sortie');
+
+        // Use assertions to check that the properties are properly displayed.
+    }
+
+    public function testEdit(): void
+    {
+        $this->markTestIncomplete();
+        $fixture = new Sortie();
+        $fixture->setNom('Value');
+        $fixture->setDateHeureDebut('Value');
+        $fixture->setDuree('Value');
+        $fixture->setDateLimiteInscription('Value');
+        $fixture->setNbInscriptionsMax('Value');
+        $fixture->setInfosSortie('Value');
+        $fixture->setEtat('Value');
+
+        $this->manager->persist($fixture);
+        $this->manager->flush();
+
+        $this->client->request('GET', sprintf('%s%s/edit', $this->path, $fixture->getId()));
+
+        $this->client->submitForm('Update', [
+            'sortie[nom]' => 'Something New',
+            'sortie[dateHeureDebut]' => 'Something New',
+            'sortie[duree]' => 'Something New',
+            'sortie[dateLimiteInscription]' => 'Something New',
+            'sortie[nbInscriptionsMax]' => 'Something New',
+            'sortie[infosSortie]' => 'Something New',
+            'sortie[etat]' => 'Something New',
+        ]);
+
+        self::assertResponseRedirects('/sortie/');
+
+        $fixture = $this->repository->findAll();
+
+        self::assertSame('Something New', $fixture[0]->getNom());
+        self::assertSame('Something New', $fixture[0]->getDateHeureDebut());
+        self::assertSame('Something New', $fixture[0]->getDuree());
+        self::assertSame('Something New', $fixture[0]->getDateLimiteInscription());
+        self::assertSame('Something New', $fixture[0]->getNbInscriptionsMax());
+        self::assertSame('Something New', $fixture[0]->getInfosSortie());
+        self::assertSame('Something New', $fixture[0]->getEtat());
+    }
+
+    public function testRemove(): void
+    {
+        $this->markTestIncomplete();
+        $fixture = new Sortie();
+        $fixture->setNom('Value');
+        $fixture->setDateHeureDebut('Value');
+        $fixture->setDuree('Value');
+        $fixture->setDateLimiteInscription('Value');
+        $fixture->setNbInscriptionsMax('Value');
+        $fixture->setInfosSortie('Value');
+        $fixture->setEtat('Value');
+
+        $this->manager->persist($fixture);
+        $this->manager->flush();
+
+        $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+        $this->client->submitForm('Delete');
+
+        self::assertResponseRedirects('/sortie/');
+        self::assertSame(0, $this->repository->count([]));
+    }
+}
