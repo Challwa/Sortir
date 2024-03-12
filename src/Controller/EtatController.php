0a93afb7a44e4c7c1fae18953532da47bd2cbd2a
LiseF4 <lise.fesselier@gmail.com>
2024-03-12 13:39:10 +0100

    #add formulaire inscription user

--- src/Controller/EtatController.php
+++ src/Controller/EtatController.php
@@ -14,77 +14,12 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[Route('/etat')]
 class EtatController extends AbstractController
 {
-    #[Route('/index', name: 'index', methods: ['GET'])]
-
-    public function index(EntityManagerInterface $entityManager): Response
+    #[Route('/etat', name: 'etat', methods: ['GET'])]
+    public function etat(): Response
     {
-        $etats = $entityManager
-            ->getRepository(Etat::class)
-            ->findAll();
-
         return $this->render('etat/index.html.twig', [
-            'etats' => $etats,
+            'controller_name' => 'EtatController',
         ]);
     }
 
-    #[Route('/create', name: 'create', methods: ['GET', 'POST'])]
-    /*
-  * ne pas oublier le
-  #[IsGranted('ROLE_USER')]
-  * */
-    public function create(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $etat = new Etat();
-        $form = $this->createForm(EtatType::class, $etat);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($etat);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_etat_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('etat/new.html.twig', [
-            'etat' => $etat,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'read', methods: ['GET'])]
-    public function read(Etat $etat): Response
-    {
-        return $this->render('etat/show.html.twig', [
-            'etat' => $etat,
-        ]);
-    }
-
-    #[Route('/{id}/update', name: 'update', methods: ['GET', 'POST'])]
-    public function update(Request $request, Etat $etat, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(EtatType::class, $etat);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_etat_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('etat/edit.html.twig', [
-            'etat' => $etat,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_etat_delete', methods: ['POST'])]
-    public function delete(Request $request, Etat $etat, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$etat->getId(), $request->request->get('_token'))) {
-            $entityManager->remove($etat);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('app_etat_index', [], Response::HTTP_SEE_OTHER);
-    }
 }
--- src/Controller/VilleController.php
+++ src/Controller/VilleController.php
@@ -13,76 +13,19 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/ville')]
 class VilleController extends AbstractController
 {
-    #[Route('/index', name: 'index', methods: ['GET'])]
-    public function index(EntityManagerInterface $entityManager): Response
-    {
-        $villes = $entityManager
-            ->getRepository(Ville::class)
-            ->findAll();
 
-        return $this->render('ville/index.html.twig', [
-            'villes' => $villes,
-        ]);
-    }
 
-    #[Route('/create', name: 'create', methods: ['GET', 'POST'])]
+    #[Route('/ville', name: 'ville', methods: ['GET', 'POST'])]
     /*
   * ne pas oublier le
   #[IsGranted('ROLE_USER')]
   * */
-    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    public function ville(): Response
     {
-        $ville = new Ville();
-        $form = $this->createForm(VilleType::class, $ville);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($ville);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_ville_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('ville/new.html.twig', [
-            'ville' => $ville,
-            'form' => $form,
+        return $this->render('ville/index.html.twig', [
+            'controller_name' => 'VilleController',
         ]);
     }
 
-    #[Route('/{id}', name: 'read', methods: ['GET'])]
-    public function read(Ville $ville): Response
-    {
-        return $this->render('ville/show.html.twig', [
-            'ville' => $ville,
-        ]);
-    }
 
-    #[Route('/{id}/edit', name: 'app_ville_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Ville $ville, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(VilleType::class, $ville);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_ville_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('ville/edit.html.twig', [
-            'ville' => $ville,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_ville_delete', methods: ['POST'])]
-    public function delete(Request $request, Ville $ville, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$ville->getId(), $request->request->get('_token'))) {
-            $entityManager->remove($ville);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('app_ville_index', [], Response::HTTP_SEE_OTHER);
-    }
 }
