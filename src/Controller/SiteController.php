d7d21bcdaf71c061aa01a96a780600f7a88674b5
ggautier2023 <gaelle.gautier@netcourrier.com>
2024-03-13 15:42:03 +0100

    Change SiteController Install composer require twig/string-extra Create edit.html.twig Change site + base.html.twig

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -8,14 +8,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route(path: 'sites/', name: 'app_')]
+#[Route(path: 'sites/', name: 'app_sites_')]
 class SiteController extends AbstractController
 {
-    #[Route(path: '', name: 'sites')]
+    #[Route(path: '', name: '')]
     public function sites(EntityManagerInterface $entityManager): Response
     {
         $sites = $entityManager->getRepository(Site::class)->findAll();
 
-        return $this->render('home/sites.html.twig', compact('sites'));
+        return $this->render('sites/sites.html.twig', compact('sites'));
     }
+
+    #[Route(path: '{id}', name: 'edit')]
+    public function edit(int $id, EntityManagerInterface $entityManager): Response
+    {
+        $detail = $entityManager->getRepository(Site::class)->findOneBy([
+            'id' => $id
+        ]);
+
+        return $this->render('sites/edit.html.twig', compact('detail'));
+    }
+
+
 }
