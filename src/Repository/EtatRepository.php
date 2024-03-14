1ab7704182b751621914825a16db1bd75a7c9d16
ddelascasas2023 <daniel.delascasas2023@campus-eni.fr>
2024-03-14 13:46:35 +0100

    Relation Etat-Sortie Etat Repository Modif sur Sortie Modif sur SortirController pour avoir Etat au moment creation d Sortie Modif Formulaire pour avout donnes par default

---
+++ src/Repository/EtatRepository.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Etat;
+use App\Entity\Site;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Etat>
+ *
+ * @method Etat|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Etat|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Etat[]    findAll()
+ * @method Etat[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class EtatRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Etat::class);
+    }
+
+    //    /**
+    //     * @return Site[] Returns an array of Site objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('s')
+    //            ->andWhere('s.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('s.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Site
+    //    {
+    //        return $this->createQueryBuilder('s')
+    //            ->andWhere('s.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+}
