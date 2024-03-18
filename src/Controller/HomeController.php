e298dc5bd784c0731c31880d000770f4d8f5d379
ggautier2023 <gaelle.gautier@netcourrier.com>
2024-03-18 11:39:48 +0100

    Création d'un formulaire pour la recherche sur page d'accueil Modif du homecontroller + template twig index + app.css en conséquence

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,10 +4,12 @@ namespace App\Controller;
 
 use App\Entity\Site;
 use App\Entity\Sortie;
+use App\Form\SearchType;
 use App\Repository\ParticipantRepository;
 use App\Repository\SortieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -15,13 +17,23 @@ class HomeController extends AbstractController
 {
     #[Route(path: "")]
     #[Route('/home', name: 'app_home', methods : ['GET'])]
-    public function home(EntityManagerInterface $entityManager, SortieRepository $sortieRepository, ParticipantRepository $participantRepository): Response
+    public function home(Request $request, EntityManagerInterface $entityManager, SortieRepository $sortieRepository, ParticipantRepository $participantRepository): Response
     {
+
+        $form = $this->createForm(SearchType::class);
+//        $form->handleRequest($request);
+
         $sites = $entityManager->getRepository(Site::class)->findAll();
+        foreach ($sites as $site){
+            $site->setNom(strtoupper($site->getNom()));
+        }
+
         $sorties = $sortieRepository->findAllWithEtat();
         $organisateur = $participantRepository->findAllWithOrganisateur();
-        return $this->render('home/index.html.twig', compact('sites', 'sorties', 'organisateur'));
+
+        return $this->render('home/index.html.twig', compact('sites', 'sorties', 'organisateur', 'form'));
     }
+
 
    #[Route(path: "profil", name: "app_profil", methods: ["GET"])]
     public function profil(): Response
