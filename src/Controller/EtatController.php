b7d82868015ff7604551da40f4c8a6a83ea2ea0e
ddelascasas2023 <daniel.delascasas2023@campus-eni.fr>
2024-03-19 12:03:05 +0100

    Creation service Sortie + Ajoute dans le controller EtatController , il faut encore gerer la function pour update en login

--- src/Controller/EtatController.php
+++ src/Controller/EtatController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Etat;
+use App\Entity\Sortie;
 use App\Form\EtatType;
+use App\Service\SortieService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,12 +16,26 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[Route('/etat')]
 class EtatController extends AbstractController
 {
-    #[Route('/etat', name: 'etat', methods: ['GET'])]
-    public function etat(): Response
+  private $sortieService;
+
+    public function getSortieService(): SortieService
     {
-        return $this->render('etat/index.html.twig', [
-            'controller_name' => 'EtatController',
-        ]);
+        return $this->sortieService;
     }
 
+  public function __construct(SortieService $sortieService)
+  {
+      $this->sortieService = $sortieService;
+  }
+
+    #[Route('/update', name: 'update_etats')]
+    public function updateEtatsOnLogin(EntityManagerInterface $entityManager,Sortie $sortie): Response
+    {
+        $user = $this->getUser();
+        if ($user) {
+            $this->sortieService->updateEtatSortie(); //appel de la fonction updateEtats -> SortieService.php
+        }
+
+        return $this->redirectToRoute('app_home');
+    }
 }
---
+++ src/Service/SortieService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Service;
+use App\Entity\Etat;
+use App\Entity\Sortie;
+use Doctrine\ORM\EntityManagerInterface;
+
+class SortieService
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function updateEtatSortie(): void
+    {
+
+        $sorties = $this->entityManager->getRepository(Sortie::class)->findAll();
+
+        foreach ($sorties as $sortie) {
+            $dateFinSortie = clone $sortie->getDateHeureDebut();
+            $dateFinSortie->modify('+ ' . $sortie->getDuree() . ' minutes');
+
+            if ($dateFinSortie < new \DateTime()) {
+                $etat = $this->entityManager->getRepository(Etat::class)->findOneBy(['libelle' => 'activite passee']);
+                $sortie->setEtats($etat);
+            }
+        }
+
+        $this->entityManager->flush();
+    }
+}
