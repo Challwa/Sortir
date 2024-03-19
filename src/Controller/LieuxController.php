8181c73726dea7f56c71b3dc1f7555ad9bc6e5a6
ddelascasas2023 <daniel.delascasas2023@campus-eni.fr>
2024-03-19 16:53:27 +0100

    Ajax latitude longitude

---
+++ src/Controller/LieuxController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Lieu;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/lieu')]
+class LieuxController extends AbstractController
+{
+    #[Route('/info/{id}', name: 'lieu_info')]
+    public function getInfoLieu(Lieu $lieu): JsonResponse
+    {
+        $information = [
+            'ville' => $lieu->getVilles(),
+            'rue' => $lieu->getRue(),
+            'latitude' => $lieu->getLatitude(),
+            'longitude' => $lieu->getLongitude(),
+        ];
+        return new JsonResponse($information);
+    }
+}
